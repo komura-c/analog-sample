c51890b8d1ef0edc683bdbc797600161bfccb773
komura-c <affirman03139@outlook.jp>
2022-12-11 02:17:01 +0900

    fix: remove read-the-docs

--- src/app/routes/(home).ts
+++ src/app/routes/(home).ts
@@ -19,18 +19,8 @@ export const routeMeta = defineRouteMeta({
         >Analog</a
       >, the fullstack meta-framework for Angular powered by Vite!
     </p>
-
-    <p class="read-the-docs">
-      Click on the Vite and Angular logos to learn more.
-    </p>
   `,
-  styles: [
-    `
-      .read-the-docs {
-        color: #888;
-      }
-    `,
-  ],
+  styles: [],
 })
 export default class HomeComponent {
   count = 0;
